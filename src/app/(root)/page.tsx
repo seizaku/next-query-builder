37309e8108ceed8034ebde12b0be59ba9d068875
zernalandrei <landreizerna@yeoworks.io>
2024-07-20 05:41:49 +0800

    feat: home page

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,37 @@
+import { UserDataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { User } from "./_components/columns";
+import { Button } from "@/components/ui/button";
+import { CustomQueryBuilder } from "./_components/query-builder";
+import { Query } from "./_components/query";
+
+async function fetchData() {
+  const res = await fetch("http://localhost:3000/api/users");
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json() as Promise<{ data: User[] }>;
+}
+
+export default async function Home() {
+  const user = await fetchData();
+
+  return (
+    <main className="min-h-screen p-8 sm:p-24">
+      <Query />
+      <section>
+        <h1 className="py-2 text-xl font-bold">Users</h1>
+        <h6 className="mb-4 text-xs font-medium text-muted-foreground">
+          {user.data.length} Users
+        </h6>
+        <CustomQueryBuilder />
+      </section>
+      <section className="mt-6">
+        <UserDataTable columns={columns} data={user.data} />
+      </section>
+    </main>
+  );
+}
