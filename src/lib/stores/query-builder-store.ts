f089ad7e197a69c7a9e6cffe33e57ff7ad122a1e
zernalandrei <landreizerna@yeoworks.io>
2024-07-20 03:51:03 +0800

    feat: add query builder store

---
+++ src/lib/stores/query-builder-store.ts
@@ -0,0 +1,69 @@
+import { RuleGroupType } from "react-querybuilder";
+import { create } from "zustand";
+import { fields, getOperators } from "@/app/(root)/_components/query-builder";
+
+type QueryStoreProps = {
+  query?: any;
+  addRule: (fieldName: string) => void;
+  setCombinator: (combinator: "and" | "or") => void;
+  setRuleField: (index: number, fieldName: string) => void;
+  setRuleOperator: (index: number, operator: string) => void;
+  setRuleValue: (index: number, value: string) => void;
+  deleteRule: (index: number) => void;
+};
+
+export const QueryBuilderStore = create<QueryStoreProps>((set, get) => ({
+  query: {
+    combinator: "and",
+    rules: [{ field: "name", operator: "=", value: "Steve" }],
+  },
+  addRule: (fieldName) => {
+    const field = fields.find((item) => item.name === fieldName);
+    const query = get().query;
+    const operator = getOperators(fieldName)[0].name;
+    switch (field?.inputType) {
+      case "text":
+        query.rules.push({ field: fieldName, operator, value: "" });
+        break;
+    }
+    set({
+      query,
+    });
+  },
+  setCombinator: (combinator) => {
+    const query = get().query;
+    query.combinator = combinator;
+    set({
+      query,
+    });
+  },
+  setRuleField: (index, fieldName) => {
+    const query = get().query;
+    const operator = getOperators(fieldName)[0].name;
+    query.rules[index] = { field: fieldName, operator, value: "" };
+    set({
+      query,
+    });
+  },
+  setRuleOperator: (index, operator) => {
+    const query = get().query;
+    query.rules[index].operator = operator;
+    set({
+      query,
+    });
+  },
+  setRuleValue: (index, value) => {
+    const query = get().query;
+    query.rules[index].value = value;
+    set({
+      query,
+    });
+  },
+  deleteRule: (index: number) => {
+    const query = get().query;
+    query.rules.splice(index, 1);
+    set({
+      query,
+    });
+  },
+}));
