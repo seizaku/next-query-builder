9fa2d8f633bba4169293f62ae4fdef1475ae28a5
zernalandrei <landreizerna@yeoworks.io>
2024-07-20 06:05:49 +0800

    fix: fetch error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: "https://seizaku-query-builder.vercel.app",
+  },
+};
 
 export default nextConfig;
