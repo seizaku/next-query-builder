169e3677e266a817543b384baea3cc512a8c268c
zernalandrei <landreizerna@yeoworks.io>
2024-07-21 02:26:44 +0800

    feat: add show query section in json format

--- src/app/_components/query.tsx
+++ src/app/_components/query.tsx
@@ -1,20 +1,23 @@
 "use client";
 
+import prisma from "@/lib/db/client";
 import { QueryBuilderStore } from "@/lib/stores/query-builder-store";
 import { useEffect } from "react";
 import { formatQuery } from "react-querybuilder";
+import { User } from "./columns";
+import { UserDataStore } from "@/lib/stores/user-data-store";
 
 export function Query() {
   const { query } = QueryBuilderStore();
 
-  useEffect(() => {
-    console.log(formatQuery(query, "sql"));
-  }, [query]);
-
   return (
     <section className="mb-4 rounded-xl border p-4">
       <h1 className="text-sm font-semibold">SQL Query:</h1>
-      <p className="mt-2 text-sm">{formatQuery(query, "sql")}</p>
+      <pre>
+        <p className="mt-2 rounded-xl bg-muted p-4 text-sm">
+          {formatQuery(query, "json")}
+        </p>
+      </pre>
     </section>
   );
 }
