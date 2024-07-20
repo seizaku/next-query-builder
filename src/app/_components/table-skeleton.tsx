04b11f0ab213bee4f92f99bb8a09b4fb534655e8
zernalandrei <landreizerna@yeoworks.io>
2024-07-21 02:57:34 +0800

    fix: add key prop to skeleton row

--- src/app/_components/table-skeleton.tsx
+++ src/app/_components/table-skeleton.tsx
@@ -5,8 +5,8 @@ import { columns } from "./columns";
 export function TableLoadingSkeleton() {
   return (
     <>
-      {Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9]).map(() => (
-        <TableRow>
+      {Array.from([1, 2, 3, 4, 5, 6, 7, 8, 9]).map((value) => (
+        <TableRow key={`skeleton-${value}`}>
           <TableCell colSpan={columns.length} className="text-center">
             <Skeleton className="h-8 w-full" />
           </TableCell>
