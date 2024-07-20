e00b30bdc51ee18dd6cf5d927d4804ae897c8b75
zernalandrei <landreizerna@yeoworks.io>
2024-07-21 02:57:54 +0800

    feat: backend route

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,24 +1,41 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import prisma from '@/lib/db/client'
+import { Hono } from 'hono';
+import { handle } from 'hono/vercel';
+import prisma from '@/lib/db/client';
+import { convertToPrismaQuery } from "@/lib/helpers/query-to-prisma";
 
-// export const runtime = 'edge'
+// Initialize the Hono app with a base path of '/api'
+const app = new Hono().basePath('/api');
 
-const app = new Hono().basePath('/api')
-
-
-// Fetch users
+// Fetch users endpoint
 app.get('/users', async (c) => {
-  const users = await prisma.user.findMany({
-    include: {
-      profile: true
-    }
-  });
+  // Get the query parameter from the request
+  const query = c.req.query('query');
   
-  return c.json({
-    data: users
-  })
-})
+  // If a query is provided, convert it to a Prisma query and fetch users based on the converted query
+  if (query) {
+    const prismaQuery = convertToPrismaQuery(JSON.parse(decodeURIComponent(query || "")));
+    
+    const users = await prisma.user.findMany({
+      where: prismaQuery,
+      include: {
+        profile: true // Include related profile information
+      },
+    });
+    return c.json({
+      data: users
+    });
+  } else {
+    // If no query is provided, fetch all users with their profile information
+    const users = await prisma.user.findMany({
+      include: {
+        profile: true // Include related profile information
+      },
+    });
+    return c.json({
+      data: users
+    });
+  }
+});
 
-export const GET = handle(app)
-export const POST = handle(app)
+// Export the GET handler for the Vercel deployment
+export const GET = handle(app);
