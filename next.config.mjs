0abc49cd9b2369ad25ce6e26a4426b2774df6187
zernalandrei <landreizerna@yeoworks.io>
2024-07-21 02:58:49 +0800

    fix: remove env nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL: "https://seizaku-query-builder.vercel.app",
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
