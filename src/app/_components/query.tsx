404a88b2686a82c11b01a8d3a90622d047f9268e
zernalandrei <landreizerna@yeoworks.io>
2024-07-21 18:24:31 +0800

    refactor: remove unused components

--- src/app/_components/query.tsx
+++ src/app/_components/query.tsx
@@ -1,11 +1,7 @@
 "use client";
 
-import prisma from "@/lib/db/client";
 import { QueryBuilderStore } from "@/lib/stores/query-builder-store";
-import { useEffect } from "react";
 import { formatQuery } from "react-querybuilder";
-import { User } from "./columns";
-import { UserDataStore } from "@/lib/stores/user-data-store";
 
 export function Query() {
   const { query } = QueryBuilderStore();
