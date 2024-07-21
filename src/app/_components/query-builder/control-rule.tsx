c3bb43a05554437d7f554d20aabcf13bb0e6c3db
zernalandrei <landreizerna@yeoworks.io>
2024-07-22 05:04:21 +0800

    feat: add clear all query

--- src/app/_components/query-builder/control-rule.tsx
+++ src/app/_components/query-builder/control-rule.tsx
@@ -4,18 +4,20 @@ import { QueryBuilderStore } from "@/lib/stores/query-builder-store";
 import { PlusIcon } from "@radix-ui/react-icons";
 
 export function ControlRule() {
-  const { query, addGroup } = QueryBuilderStore();
+  const { query, addGroup, clearAll } = QueryBuilderStore();
   return (
-    <section className="flex justify-start py-2">
+    <section className="flex w-full justify-start gap-2 py-2">
       {!!query.rules.length && (
         <Button onClick={() => addGroup([])} variant={"ghost"}>
           <PlusIcon className="mr-2 h-5" />
           Group
         </Button>
       )}
-      {/* <Button onClick={clearAll} variant={"ghost"}>
-        Clear all
-      </Button> */}
+      {!!query.rules.length && (
+        <Button onClick={clearAll} variant={"secondary"}>
+          Clear all
+        </Button>
+      )}
     </section>
   );
 }
