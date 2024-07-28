b01a7f44c8ba67c31fe5b758dc36eb790e654b6e
zernalandrei <landreizerna@yeoworks.io>
2024-07-29 05:06:23 +0800

    feat: group control

---
+++ src/app/(root)/_components/query-builder/GroupControl.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { QueryBuilderStore } from "@/lib/stores/query-store";
+import { PlusIcon } from "@radix-ui/react-icons";
+
+export function GroupControl() {
+  const { query, addGroup, clearAll } = QueryBuilderStore();
+  return (
+    <section className="flex w-full justify-start gap-2 py-2">
+      {query.rules.length > 0 && (
+        <Button onClick={() => addGroup([])} variant={"ghost"}>
+          <PlusIcon className="mr-2 h-5" />
+          Group
+        </Button>
+      )}
+      {query.rules.length && (
+        <Button onClick={clearAll} variant={"secondary"}>
+          Clear all
+        </Button>
+      )}
+    </section>
+  );
+}
