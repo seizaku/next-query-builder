0075ce9f4074e27d3210e8a55e6645249f6e1d6e
zernalandrei <landreizerna@yeoworks.io>
2024-07-29 05:04:15 +0800

    feat: display query in sql format

---
+++ src/app/(root)/_components/DisplayQuery.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { parseRules } from "@/lib/helpers/parse-rules";
+import { QueryBuilderStore } from "@/lib/stores/query-store";
+import { formatQuery } from "react-querybuilder";
+
+export function Query() {
+  const { query } = QueryBuilderStore();
+
+  return (
+    <section className="mb-4 rounded-xl border p-4">
+      <h1 className="text-sm font-semibold">SQL Query:</h1>
+      <pre className="mt-4 text-wrap">
+        <p className="mt-2 rounded-xl bg-muted p-4 text-sm">
+          {formatQuery(parseRules(query) as any as any, "sql")}
+        </p>
+      </pre>
+    </section>
+  );
+}
