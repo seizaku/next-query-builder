ecdbb4002db1985ab43894cf94556ec83d61487b
zernalandrei <landreizerna@yeoworks.io>
2024-07-30 04:26:10 +0800

    check for empty values before fetching

--- src/app/_components/Users.tsx
+++ src/app/_components/Users.tsx
@@ -7,7 +7,7 @@ import { DataTable } from "./datatable/DataTable";
 import { columns } from "./datatable/Columns";
 import { QueryBuilderStore } from "@/lib/stores/query-store";
 import { UserStore } from "@/lib/stores/user-store";
-import { formatQuery } from "react-querybuilder";
+import { formatQuery, RuleType } from "react-querybuilder";
 import { parseRules } from "@/lib/helpers/parse-rules";
 
 import { User, Profile } from "@/types/index";
@@ -20,13 +20,31 @@ export function Users({ initialData }: { initialData: Array<User & Profile> }) {
     mutationFn: getRecords,
   });
 
-  useEffect(() => {
+  const refetch = () => {
+    const checkEmptyValues = (array: any) =>
+      "rules" in array &&
+      array.rules.some(
+        (y: any) =>
+          "value" in y &&
+          (y.value == "" || y.value == undefined || y.value == null),
+      );
+
+    const hasEmptyValues = query.rules.length
+      ? query.rules.some((x) => checkEmptyValues(x))
+      : true;
+
+    if (hasEmptyValues) return;
+
     const formattedQuery = JSON.stringify(
       formatQuery(parseRules(query as any) as any, {
         format: "parameterized",
       }),
     );
     mutate(formattedQuery);
+  };
+
+  useEffect(() => {
+    refetch();
   }, [JSON.stringify(query.rules)]);
 
   // Set initial record on first server side fetch
